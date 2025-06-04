2c547946fa09ad87cf06dd438a66114d5b04b9bb
DoDavin <dodavin96@gmail,com>
2025-06-04 19:47:36 +0700

    Created Functions Structure

--- Project/Features.cpp
+++ Project/Features.cpp
@@ -1,1 +1,77 @@
 // Kheang Ann and Sok Leap
+
+struct Product {
+    // Put Attribute
+    Product *next;
+};
+
+struct List {
+    // Put Attribute
+    int n;
+    Product *head;
+    Product *tail;
+};
+
+List *createList(){
+    List *ls = new List;
+    ls->n = 0;
+    ls->head = nullptr;
+    ls->tail = nullptr;
+    return ls;
+}
+
+// Add Product
+void addBeg(List *ls){
+
+}
+
+void addEnd(List *ls){
+
+}
+
+void addPos(List *ls, int pos){
+
+}
+
+// Update Product
+void updateProduct(List *ls){
+
+}
+
+// Delete Product
+void deleteBeg(List *ls){
+
+}
+
+void deleteEnd(List *ls){
+
+}
+
+void deletePos(List *ls){
+
+}
+
+// Reverse List
+void reverseList(List *ls){
+
+}
+
+// Combine List
+void combine(List *ls){
+
+}
+
+// Search Product by ID
+int searchProduct(List *ls){
+    return 0;
+}
+
+// Sort Product using Bubble Sort Algorithm
+void bubbleSort(List *ls){
+
+}
+
+// Display Product
+void display(List *ls){
+
+}
