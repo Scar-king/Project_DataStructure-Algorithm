0b1c50a93330825dbc9f75a9a21878edbb5729bd
DoDavin <dodavin96@gmail,com>
2025-06-08 00:56:45 +0700

    Testing Bubble Sort

--- Project/test/testSort.cpp
+++ Project/test/testSort.cpp
@@ -1,27 +1,88 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
 using namespace std;
 
-void sort(string name[], int n){
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n - 1 - i; j++){
-            if(name[j] > name[j + 1]){
-                string temp = name[j];
-                name[j] = name[j + 1];
-                name[j + 1] = temp;
-            }
-        }
+struct Product {
+    double price;
+    Product *next;
+    Product *prev;
+};
+
+struct List {
+    int n;
+    Product *head;
+    Product *tail;
+};
+
+List *create(){
+    List *ls = new List;
+    ls->n = 0;
+    ls->head = nullptr;
+    ls->tail = nullptr;
+    return ls;
+}
+
+void addBeg(List *ls, double price){
+    Product *p = new Product;
+    p->price = price;
+    p->next = ls->head;
+    ls->head = p;
+    p->prev = nullptr;
+    if(ls->n == 0){
+        ls->tail = p;
     }
+    ls->n++;
+}
+
+void display(List *ls){
+    Product *temp = ls->head;
+    while(temp != nullptr){
+        cout << temp->price << "$ ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+void sort(List *ls){
+    if(ls->n < 2) return;
+    Product *ptr;
+    Product *lastptr = nullptr;
+    bool swapped;
+    do {
+        ptr = ls->head;
+        swapped = false;
+        while(ptr->next != lastptr){
+            if(ptr->price > ptr->next->price){
+                double temp = ptr->price;
+                ptr->price = ptr->next->price;
+                ptr->next->price = temp;
+                swapped = true;
+            }
+            ptr = ptr->next;
+        }
+        lastptr = ptr;
+    } while(swapped);
 }
 
 int main(){
 
-    string name[3] = {"Davin", "Thyda", "Layla"};
+    List *ls = create();
 
-    sort(name, 3);
+    srand(time(0));
 
-    for(string s : name){
-        cout << s << endl;
+    for(int i = 0; i <= 10; i++){
+        double price = rand() % 10 + 1;
+        addBeg(ls, price);
     }
+
+    cout << "Unsort: ";
+    display(ls);
+
+    sort(ls);
+
+    cout << "\nsorted: ";
+    display(ls);
 
     return 0;
 }
