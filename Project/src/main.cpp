31de5374eed17209399e68272bb0690294e26e8b
SAM Sokleap <leapngu2023@gmail.com>
2025-06-11 07:54:27 +0700

    Test addEnd of the list, and deletePos of the list

--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -12,9 +12,12 @@ int main() {
     List *myList = createList();
 
     addBeg(myList, "iPhone", 1, 10.0); 
-
+    addEnd(myList, "Samsung", 1, 99.99);
+    addEnd(myList, "Oppo", 1, 1.99);
     cout << "\nList: ";
     displayBeg(myList);
+    deletePos(myList, 0);
+    deletePos(myList, 1);
 
     return 0;
 }
