0fa72b0e83dd1c996f1ec46c2ac5243ad90c911a
KheangAnn <Kheangann@gmail.com>
2025-06-13 21:00:06 +0700

    another style of loading

--- Project/test/testLoading.cpp
+++ Project/test/testLoading.cpp
@@ -1,32 +1,60 @@
 #include <iostream>
 #include <windows.h>
+#include <stdio.h>
 using namespace std;
 
-#define RESET   "\033[0m"
-#define RED     "\033[31m"
-#define GREEN   "\033[32m"
-#define YELLOW  "\033[33m"
-#define BLUE    "\033[34m"
-#define WHITE   "\033[37m"
+#define RESET "\033[0m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define WHITE "\033[37m"
 
 void loading(int width = 50, int delay = 50){
     cout << YELLOW << "[";
 
-    for(int i = 0; i < width; i++){
+    for (int i = 0; i < width; i++){
         cout << "-";
     }
     cout << "]\r["; // It goes back to the beginning and write the new one
-    for(int i = 0; i <= width; i++){
+    for (int i = 0; i <= width; i++){
         cout << GREEN << "=";
         Sleep(delay);
     }
     cout << YELLOW << "]" << RESET << endl;
     cout << "Loading Complete!\n";
 }
+// another loading style
+void loading2(){
+    system("COLOR 0e");
+    system("cls");
+    //printf("\e[?251");
+
+    // We use ASCII to print special character
+    SetConsoleCP(437);
+    SetConsoleOutputCP(437);
+    int bar1 = 177, bar2 = 219;
+
+    cout << "\n\n\n\t\t\t\tLoading...";
+    cout << "\n\n\n\t\t\t\t";
+
+    for (int i = 0; i < 25; i++){
+        cout << (char)bar1;
+    }
+    cout << "\r";
+    cout << "\t\t\t\t";
+    for (int i = 0; i < 25; i++){
+        cout << (char)bar2;
+        Sleep(150);
+    }
+    cout << "\n\t\t\t\t" << (char)1 << "!";
+    system("Pause");
+}
 
 int main(){
 
-    loading();
+    // loading();
+    loading2();
 
     return 0;
 }
