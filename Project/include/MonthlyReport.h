e7bfb8dcf1037e0771e410b2841a59e5bff7d221
DoDavin <dodavin96@gmail,com>
2025-06-16 22:56:50 +0700

    Implemented MonthlyReport, and Testing

--- Project/include/MonthlyReport.h
+++ Project/include/MonthlyReport.h
@@ -1,1 +1,76 @@
 // Davin
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+struct ReportElement {
+    int id;
+    string model;
+    int totalStock;
+    int sold;
+    double totalPurchaseCost;
+    double totalEarn;
+    double profit;
+    double estimatProfit;
+    ReportElement *next;
+    ReportElement *prev;
+};
+
+struct ReportList {
+    int n;
+    ReportElement *head;
+    ReportElement *tail;
+};
+
+ReportList *createReportList(){
+    ReportList *ls = new ReportList;
+    ls->n = 0;
+    ls->head = nullptr;
+    ls->tail = nullptr;
+    return ls;
+}
+
+void addReport(ReportList *ls, string model, int totalStock, int sold, double totalPurchaseCost, double totalEarn, double profit, double estimatProfit){
+    ReportElement *re = new ReportElement;
+    re -> id = 1000 + ls -> n;
+    re -> model = model;
+    re -> totalStock = totalStock;
+    re -> sold = sold;
+    re -> totalPurchaseCost = totalPurchaseCost;
+    re -> totalEarn = totalEarn;
+    re -> profit = profit;
+    re -> estimatProfit = estimatProfit;
+    re -> next = nullptr;
+    re -> prev = ls -> tail;
+
+    // totalStock = qtyIn - qtyOut;
+    // sold = qtyOut;
+    // totalPurchaseCost = costPrice * sold;
+    // totalEarn = price * sold;
+    // profit = totalEarn - totalPurchaseCost;
+    // estimatProfit = profit + (totalStock * (price - costPrice));
+
+    if(ls -> n == 0){
+        ls -> head = re;
+    }
+    else{
+        ls -> tail -> next = re; 
+    }
+    ls -> tail = re;
+    ls -> n++;
+}
+
+void displayMonthlyReport(ReportList *ls) { // Davin
+    ReportElement *temp = ls->head;
+    cout << "\n+------+-----------------+--------------+-------+----------------------+-------------+-------------+------------------+\n";
+    printf("| %-4s | %-15s | %-12s | %-5s | %-20s | %-11s | %-11s | %-16s |\n",
+           "ID", "MODEL", "TOTAL STOCK", "SOLD", "TOTAL PURCHASE COST", "TOTAL EARN", "PROFIT", "ESTIMATE PROFIT");
+    cout << "+------+-----------------+--------------+-------+----------------------+-------------+-------------+------------------+\n";
+    while (temp != nullptr) {
+        printf("| %-4d | %-15s | %-12d | %-5d | %-20.2f | %-11.2f | %-11.2f | %-16.2f |\n",
+               temp->id, temp->model.c_str(), temp->totalStock, temp->sold,
+               temp->totalPurchaseCost, temp->totalEarn, temp->profit, temp->estimatProfit);
+        temp = temp->next;
+    }
+    cout << "+------+-----------------+--------------+-------+----------------------+-------------+-------------+------------------+\n";
+}
--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -1,5 +1,6 @@
 #include "../include/Design_Structure.h"
 #include "../include/ProductList.h"
+#include "../include/MonthlyReport.h"
 
 using namespace std;
 
@@ -10,11 +11,18 @@ int main() {
     // menu();
 
     List *myList = createList();
+    ReportList *rl = createReportList();
+
     addEnd(myList, "Iphone 11", 10, 10, "IOS, Ram 8GB, Storage 256GB", 1111.10);
     addEnd(myList, "Iphone 12", 3, 10, "IOS, Ram 8GB, Storage 256GB", 1111.10);
     addEnd(myList, "Iphone 13", 10, 9, "IOS, Ram 8GB, Storage 256GB", 1111.10);
     displayAdminProductList(myList);
     cout <<endl<<endl;
     displayUserProductList(myList);
+
+    addReport(rl, "iPhone 14 Pro", 47, 50, 1000.00, 2000.00, 3000.00, 5000.00);
+    addReport(rl, "iPhone 13 Pro", 47, 50, 1000.00, 2000.00, 3000.00, 5000.00);
+    addReport(rl, "iPhone X", 47, 50, 1000.00, 2000.00, 3000.00, 5000.00);
+    displayMonthlyReport(rl);
     return 0;
 }
