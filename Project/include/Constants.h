3c239dd75c53a2a3d9646ad3ec9f432d43fa7f6e
DoDavin <dodavin96@gmail,com>
2025-07-20 23:17:19 +0700

    Declare INDENT & TAB for shared across files

---
+++ Project/include/Constants.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <string>
+using namespace std;
+
+// Use inline so this can be shared across files without linker errors
+inline const string INDENT = "\t\t\t\t\t\t\t\t";
+inline const string TAB = "\t\t\t\t\t";
