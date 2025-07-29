20deb220576c1ca3524cbec8855cf520de5074ff
DoDavin <dodavin96@gmail,com>
2025-07-30 01:18:08 +0700

    Added MAX & MIN (ID) Range

--- Project/include/Constants.h
+++ Project/include/Constants.h
@@ -5,3 +5,12 @@ using namespace std;
 // Use inline so this can be shared across files without linker errors
 inline const string INDENT = "\t\t\t\t\t\t\t\t";
 inline const string TAB = "\t\t\t\t\t";
+
+inline int MIN_STOCK = 0;
+inline int MAX_STOCK = 1000;
+
+inline double MIN_PRICE = 1.0;
+inline double MAX_PRICE = 10000.0;
+
+inline int MIN_ID = 1000;
+inline int MAX_ID = 10000;
